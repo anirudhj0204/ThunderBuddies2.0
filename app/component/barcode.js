e865d48fb0d1d00823c0a91419a90d3834121204
Anirudh Jyosyula <anirudhj0204@gmail.com>
2023-08-16 15:46:59 +0530

    Final UI

--- app/component/barcode.js
+++ app/component/barcode.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 const BarcodeScanner = () => {
-  const [data, setData] = useState("Not Found");
+  const [data, setData] = useState("");
   const [torchOn, setTorchOn] = useState(false);
   const [apiResult, setApiResult] = useState(null);
 
@@ -31,7 +31,7 @@ const BarcodeScanner = () => {
   };
 
   useEffect(() => {
-    if (data !== "Not Found") {
+    if (data !== "") {
       callApi(data);
     }
   }, [data]);
@@ -46,30 +46,144 @@ const BarcodeScanner = () => {
           if (result) {
             setData(result.text);
           } else {
-            setData("Not Found");
+            setData("");
           }
         }}
       />
       <p>{data}</p>
-      <button onClick={() => setTorchOn(!torchOn)}>
+      {/* <button onClick={() => setTorchOn(!torchOn)}>
         Switch Torch {torchOn ? "Off" : "On"}
-      </button>
+      </button> */}
       {apiResult && (
         <div>
-          <h2>API Result:</h2>
+          {/* <h2>API Result:</h2> */}
           {apiResult && (
         <div>
-          <h2>API Result:</h2>
+          {/* <h2>API Result:</h2> */}
           {apiResult.items.map((item, index) => (
-            <div key={index}>
-              <p>EAN: {item.ean}</p>
-              <p>Title: {item.title}</p>
-              <p>Description: {item.description}</p>
-              <p>Brand: {item.brand}</p>
-              <p>Category: {item.category}</p>
-              <p>Price: {item.offers[0].price}</p>
-              <p>Merchant: {item.offers[0].merchant}</p>
-              <img src={item.images[0]} alt={`Image ${index}`} />
+            <div>
+
+
+
+              {/* ////////////////////// */}
+
+
+              
+              <div class="min-h-screen flex items-center justify-center px-4">
+                
+                <div class="max-w-4xl  bg-white w-full rounded-lg shadow-xl">
+                    <div class="p-4 border-b">
+                        <h2 class="text-2xl ">
+                            API Result
+                        </h2>
+                        <p class="text-sm text-gray-500">
+                            Product details
+                        </p>
+                    </div>
+                    <div>
+                        <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                          <p class="text-gray-600">
+                              Title
+                          </p>
+                          <p>
+                              {item.title}
+                          </p>
+                        </div>
+
+                        <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                          <p class="text-gray-600">
+                              Brand
+                          </p>
+                          <p>
+                              {item.brand}
+                          </p>
+                        </div>
+
+                        <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                          <p class="text-gray-600">
+                              Category
+                          </p>
+                          <p>
+                              {item.category}
+                          </p>
+                        </div>
+
+                        <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                          <p class="text-gray-600">
+                              Model
+                          </p>
+                          <p>
+                              {item.model}
+                          </p>
+                        </div>
+
+                        <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                          <p class="text-gray-600">
+                              Lowest Recorded Price
+                          </p>
+                          <p>
+                              $ {item.lowest_recorded_price}
+                          </p>
+                        </div>
+
+                        <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                          <p class="text-gray-600">
+                              Highest Recorded Price
+                          </p>
+                          <p>
+                              $ {item.highest_recorded_price}
+                          </p>
+                        </div>
+
+                        <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                          <p class="text-gray-600">
+                              EAN
+                          </p>
+                          <p>
+                              {item.ean}
+                          </p>
+                        </div>
+
+                        <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                          <p class="text-gray-600">
+                              Dimension
+                          </p>
+                          <p>
+                              {item.dimension}
+                          </p>
+                        </div>
+
+                        <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                          <p class="text-gray-600">
+                              Weight
+                          </p>
+                          <p>
+                              {item.weight}
+                          </p>
+                        </div>
+
+                        <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                          <p class="text-gray-600">
+                              Description
+                          </p>
+                          <p>
+                              {item.description}
+                          </p>
+                        </div>
+
+
+                    </div>
+                </div>
+              </div>
+
+
+
+              {/* ////////////////////// */}
+
+
+
+
+              {/*  */}
             </div>
           ))}
         </div>
